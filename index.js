e30d6583e0a4eb27b18eed2e5f0746755cf71411
Billy West <billywest.jm@gmail.com>
2020-06-08 19:58:21 -0400

    refactored to use express with apollo

--- index.js
+++ index.js
@@ -1,141 +1,54 @@
-const { ApolloServer } = require('apollo-server');
-const { GraphQLScalarType } = require('graphql');
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const { MongoClient } = require('mongodb');
+const { readFileSync } = require('fs');
+const expressPlayground = require('graphql-playground-middleware-express').default;
+const resolvers = require('./resolvers');
 
-var _id = 0;
+require('dotenv').config();
+var typeDefs = readFileSync('./typeDefs.graphql', 'UTF-8');
 
-var users = [
-  { githubLogin: 'mHattrup', name: 'Mike Hattrup' },
-  { githubLogin: 'gPlake', name: 'Glen Plake' },
-  { githubLogin: 'sSchmidt', name: 'Scott Schmidt' },
-];
+async function start() {
+  const app = express();
+  const MONGO_DB = process.env.DB_HOST;
 
-var photos = [
-  {
-    id: '1',
-    name: 'Dropping the Heart Chute',
-    description: 'The heart chute is one of my favorite chutes',
-    category: 'ACTION',
-    githubUser: 'gPlake',
-    created: '3-28-1977',
-  },
-  {
-    id: '2',
-    name: 'Enjoying the sunshine',
-    category: 'SELFIE',
-    githubUser: 'sSchmidt',
-    created: '1-2-1985',
-  },
-  {
-    id: '3',
-    name: 'Gunbarrel 25',
-    description: '25 laps on gunbarrel today',
-    category: 'LANDSCAPE',
-    githubUser: 'sSchmidt',
-    created: '2018-04-15T19:09:57.308Z',
-  },
-];
+  let db;
 
-var tags = [
-  { photoID: '1', userID: 'gPlake' },
-  { photoID: '2', userID: 'sSchmidt' },
-  { photoID: '2', userID: 'mHattrup' },
-  { photoID: '2', userID: 'gPlake' },
-];
-
-const typeDefs = `
-  scalar DateTime
-  type User {
-    githubLogin: ID!
-    name: String
-    avatar: String
-    postedPhotos: [Photo!]!
-    inPhotos: [Photo!]!
+  try {
+    const client = await MongoClient.connect(MONGO_DB, { useNewUrlParser: true, useUnifiedTopology: true });
+    db = client.db();
+  } catch (error) {
+    console.log(`
+    
+      Mongo DB Host not found!
+      please add DB_HOST environment variable to .env file
+      exiting...
+       
+    `);
+    process.exit(1);
   }
 
-  type Photo {
-    id: ID!
-    url: String!
-    name: String!
-    description: String
-    category: PhotoCategory!
-    postedBy: User!
-    taggedUsers: [User!]!
-    created: DateTime!
-  }
-
-  enum PhotoCategory {
-    SELFIE
-    PORTRAIT
-    ACTION
-    LANDSCAPE
-    GRAPHIC
-  }
-
-  input PostPhotoInput {
-    name: String!
-    category: PhotoCategory=PORTRAIT
-    description: String
-  }
-
-  type Query {
-    totalPhotos: Int!
-    allPhotos: [Photo!]!
-  }
-
-  type Mutation {
-    postPhoto(input: PostPhotoInput!): Photo!
-  }
-`;
-
-const resolvers = {
-  Query: {
-    totalPhotos: () => photos.length,
-    allPhotos: () => photos,
-  },
-  Mutation: {
-    postPhoto(parent, args) {
-      var newPhoto = {
-        id: _id++,
-        ...args.input,
-        created: new Date(),
-      };
-      photos.push(newPhoto);
-      return newPhoto;
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: async ({ req }) => {
+      const githubToken = req.headers.authorization;
+      const currentUser = await db.collection('users').findOne({ githubToken });
+      return { db, currentUser };
     },
-  },
-  Photo: {
-    url: (parent) => `http://yoursite.com/img/${parent.id}.jpg`,
-    postedBy: (parent) => {
-      return users.find((u) => u.githubLogin === parent.githubUser);
-    },
-    taggedUsers: (parent) =>
-      tags
-        .filter((tag) => tag.photoID === parent.id)
-        .map((tag) => tag.userID)
-        .map((userID) => users.find((u) => u.githubLogin === userID)),
-  },
-  User: {
-    postedPhotos: (parent) => {
-      return photos.filter((p) => p.githubUser === parent.githubLogin);
-    },
-    inPhotos: (parent) =>
-      tags
-        .filter((tag) => tag.userID === parent.id)
-        .map((tag) => tag.photoID)
-        .map((photoID) => photos.find((p) => p.id === photoID)),
-  },
-  DateTime: new GraphQLScalarType({
-    name: 'DateTime',
-    description: 'A valid date time value.',
-    parseValue: (value) => new Date(value),
-    serialize: (value) => new Date(value).toISOString(),
-    parseLiteral: (ast) => ast.value,
-  }),
-};
+  });
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
+  server.applyMiddleware({ app });
 
-server.listen().then(({ url }) => console.log(`GraphQL Service running on ${url}`));
+  app.get('/playground', expressPlayground({ endpoint: '/graphql' }));
+  app.get('/', (req, res) => res.end('Welcome to the PhotoShare API'));
+
+  // app.get('/', (req, res) => {
+  //   let url = `https://github.com/login/oauth/authorize?client_id=${process.env.CLIENT_ID}&scope=user`
+  //   res.end(`<a href="${url}">Sign In with Github</a>`)
+  // })
+
+  app.listen({ port: 4000 }, () => console.log(`GraphQL Service running @ http://localhost:4000${server.graphqlPath}`));
+}
+
+start();

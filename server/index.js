c0627463048e7d181e8a2b9546fc740a4e8afa8f
Billy West <billywest.jm@gmail.com>
2020-06-12 13:51:08 -0400

    updates

---
+++ server/index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const { createServer } = require('http');
+const { ApolloServer, PubSub } = require('apollo-server-express');
+const { MongoClient } = require('mongodb');
+const { readFileSync } = require('fs');
+const expressPlayground = require('graphql-playground-middleware-express').default;
+const resolvers = require('./resolvers');
+
+require('dotenv').config();
+var typeDefs = readFileSync('./typeDefs.graphql', 'UTF-8');
+
+async function start() {
+  const app = express();
+  const MONGO_DB = process.env.DB_HOST;
+  const pubsub = new PubSub();
+  let db;
+
+  try {
+    const client = await MongoClient.connect(MONGO_DB, { useNewUrlParser: true, useUnifiedTopology: true });
+    db = client.db();
+  } catch (error) {
+    console.log(`
+    
+      Mongo DB Host not found!
+      please add DB_HOST environment variable to .env file
+      
+      exiting...
+      
+    `);
+    process.exit(1);
+  }
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: async ({ req, connection }) => {
+      const githubToken = req ? req.headers.authorization : connection.context.Authorization;
+      const currentUser = await db.collection('users').findOne({ githubToken });
+      return { db, currentUser, pubsub };
+    },
+  });
+
+  server.applyMiddleware({ app });
+
+  app.get('/playground', expressPlayground({ endpoint: '/graphql' }));
+
+  app.get('/', (req, res) => {
+    let url = `https://github.com/login/oauth/authorize?client_id=${process.env.CLIENT_ID}&scope=user`;
+    res.end(`<a href="${url}">Sign In with Github</a>`);
+  });
+
+  const httpServer = createServer(app);
+  server.installSubscriptionHandlers(httpServer);
+
+  httpServer.listen({ port: 4000 }, () => console.log(`🚀 GraphQL Service running at http://localhost:4000${server.graphqlPath}`));
+}
+
+start();
